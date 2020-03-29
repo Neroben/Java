a30770da53934c067dd9989a1e3004e4a2b7a921
Neroben <43965899+Neroben@users.noreply.github.com>
2020-03-29 20:32:47 +0300

    ManyToOne
    У сообщений появились авторы

--- Spring/Проект 1(Hello World)/src/main/java/com/example/project1/controller/MainController.java
+++ Spring/Проект 1(Hello World)/src/main/java/com/example/project1/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.project1.controller;
 
 import com.example.project1.domain.Message;
+import com.example.project1.domain.User;
 import com.example.project1.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,8 +33,13 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model
+    ){
+        Message message = new Message(text, tag, user);
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
